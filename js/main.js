34c9596839a99d800e9bf910c008cecce47fdc2c
student_113 <student_113@PSXMA113s-Air.westell.com>
2018-11-17 15:27:50 -0500

    created game logic2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
+
+
+cards = ["queen", "queen", "king", "king"];
+
+cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[1];
 var cardThree = "king";
 var cardThree = "king";
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if(cardsInPlay.length === 2){
+	if(cardOne === cardTwo){
+		alert("you found a match");
+	}else{
+		alert("sorry, try again");
+	}
+}
+console.log(cardsInPlay);
