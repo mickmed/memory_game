d8218ed68034335ad209ee510d3f659ae8173608
student_113 <student_113@PSXMA113s-Air.westell.com>
2018-11-17 17:58:42 -0500

    redo logic

--- js/main.js
+++ js/main.js
@@ -1,23 +1,38 @@
-console.log("Up and running!");
-
-
 cards = ["queen", "queen", "king", "king"];
 
+// cardOne = {
+// 	rank: "queen",
+// 	suit: "hearts",
+// 	cardImage: "images/queen-of-hearts.png";
+// }
+// cardTwo = {
+// 	rank: "queen",
+// 	suit: "diamonds",
+// 	cardImage: "images/queen-of-diamonds.png";
+// }
+// cardThree = {
+// 	rank: "king",
+// 	suit: "hearts",
+// 	cardImage: "images/king-of-diamonds.png";
+// }
 cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[1];
-var cardThree = "king";
-var cardThree = "king";
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-if(cardsInPlay.length === 2){
-	if(cardOne === cardTwo){
-		alert("you found a match");
-	}else{
-		alert("sorry, try again");
-	}
+function checkForMatch() {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match!");
+    } else {
+        console.log("Sorry, try again.");
+    }
 }
-console.log(cardsInPlay);
+
+function flipCard(cardId) {
+    console.log("user flipped " + cardId);
+    cardsInPlay.push(cardId);
+    if (cardsInPlay === 2) {
+       checkForMatch();
+    }
+}
+
+flipCard(cards[0]);
+flipCard(cards[2]);
+
